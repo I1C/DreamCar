b44de7a11ee597792d015ef6adc14a5d0795ba35
Ioan Coman <coman.ioan95@gmail.com>
2021-01-25 01:13:41 +0200

    add winners feature

--- DreamCar/src/app/home/home.component.ts
+++ DreamCar/src/app/home/home.component.ts
@@ -13,6 +13,37 @@ import { CustomEventsService } from '../services/custom-events.service';
 })
 export class HomeComponent implements OnInit {
 
+  progress = true;
+
+  tire = false;
+  suspensions = false;
+  turbocharges = false;
+  exhaustPipe = false;
+  brakePads = false;
+  rearviewMirrors = false;
+  windshield = false;
+
+  winner1: any;
+  winner2: any;
+  winner3: any;
+  winner4: any;
+  winner5: any;
+  winner6: any;
+  winner7: any;
+
+  id1 = 999;
+  id2 = 999;
+  id3 = 999;
+  id4 = 999;
+  id5 = 999;
+  id6 = 999;
+  id7 = 999;
+
+  countDownDate = new Date(Date.now() + 12096e5).getTime();
+  now = new Date().getTime();
+  distance = this.countDownDate - this.now;
+  count: any;
+
   bidder = {
     ID: undefined,
     CompanyName: undefined,
@@ -35,6 +66,11 @@ export class HomeComponent implements OnInit {
         this.dataSource = JSON.parse(String(result)).dataSource;
       }
     );
+  }
+
+  setMin1(): void {
+    console.log('A intrat!!!!');
+    console.log(this.dataSource);
   }
 
   openDialog(): void {
@@ -67,8 +103,165 @@ export class HomeComponent implements OnInit {
       (data: any) => {
         this.dataSource = data;
         console.log(this.dataSource);
+        for (const i of this.dataSource){
+          const x = setInterval(() => {
+            const now = new Date().getTime();
+            const distance = this.countDownDate - now;
+            const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            const second = Math.floor((distance % (1000 * 60)) / 1000);
+            this.count = days + 'd ' + hours + 'h ' + minutes + 'm ' + second + 's ';
+            if (this.distance < 0){
+              clearInterval(x);
+              this.count = 'Expired';
+              this.progress = false;
+              // tslint:disable-next-line:triple-equals
+              if (i.OccID == 1){
+                if (i.Price < this.id1) {
+                  this.tire = true;
+                  console.log('Tire-boolean: ' + this.tire);
+                  this.id1 = i.Price;
+                  this.winner1 = i.CompanyName;
+                  console.log('Tire-winner: ' + i.CompanyName + ' with ' + i.Price + '$/Lot!');
+                }
+              }
+              // tslint:disable-next-line:triple-equals
+              if (i.OccID == 2){
+                if (i.Price < this.id2) {
+                  this.suspensions = true;
+                  console.log('Suspensions-boolean: ' + this.suspensions);
+                  this.id2 = i.Price;
+                  this.winner2 = i.CompanyName;
+                  console.log('Suspensions-winner: ' + i.CompanyName + ' with ' + i.Price + '$/Lot!');
+                }
+              }
+              // tslint:disable-next-line:triple-equals
+              if (i.OccID == 3){
+                if (i.Price < this.id3) {
+                  this.turbocharges = true;
+                  console.log('Turbocharges-boolean: ' + this.turbocharges);
+                  this.id3 = i.Price;
+                  this.winner3 = i.CompanyName;
+                  console.log('Turbocharges-winner: ' + i.CompanyName + ' with ' + i.Price + '$/Lot!');
+                }
+              }
+              // tslint:disable-next-line:triple-equals
+              if (i.OccID == 4){
+                if (i.Price < this.id4) {
+                  this.exhaustPipe = true;
+                  console.log('ExhaustPipe-boolean: ' + this.exhaustPipe);
+                  this.id4 = i.Price;
+                  this.winner4 = i.CompanyName;
+                  console.log('ExhaustPipe-winner: ' + i.CompanyName + ' with ' + i.Price + '$/Lot!');
+                }
+              }
+              // tslint:disable-next-line:triple-equals
+              if (i.OccID == 5){
+                if (i.Price < this.id5) {
+                  this.brakePads = true;
+                  console.log('BrakePads-boolean: ' + this.brakePads);
+                  this.id5 = i.Price;
+                  this.winner5 = i.CompanyName;
+                  console.log('BrakePads-winner: ' + i.CompanyName + ' with ' + i.Price + '$/Lot!');
+                }
+              }
+              // tslint:disable-next-line:triple-equals
+              if (i.OccID == 6){
+                if (i.Price < this.id6) {
+                  this.rearviewMirrors = true;
+                  console.log('RearviewMirrors-boolean: ' + this.rearviewMirrors);
+                  this.id6 = i.Price;
+                  this.winner6 = i.CompanyName;
+                  console.log('RearviewMirrors-winner: ' + i.CompanyName + ' with ' + i.Price + '$/Lot!');
+                }
+              }
+              // tslint:disable-next-line:triple-equals
+              if (i.OccID == 7){
+                if (i.Price < this.id7) {
+                  this.windshield = true;
+                  console.log('Windshield-boolean: ' + this.windshield);
+                  this.id7 = i.Price;
+                  this.winner7 = i.CompanyName;
+                  console.log('Windshield-winner: ' + i.CompanyName + ' with ' + i.Price + '$/Lot!');
+                }
+              }
+            }
+          });
+          // tslint:disable-next-line:triple-equals
+          if (i.OccID == 1){
+            if (i.Price <= 30) {
+              this.tire = true;
+              console.log('Tire-boolean: ' + this.tire);
+              this.id1 = i.Price;
+              this.winner1 = i.CompanyName;
+              console.log('Tire-winner: ' + i.CompanyName + ' with ' + i.Price + '$/Lot!');
+            }
+          }
+          // tslint:disable-next-line:triple-equals
+          if (i.OccID == 2){
+            if (i.Price <= 30) {
+              this.suspensions = true;
+              console.log('Suspensions-boolean: ' + this.suspensions);
+              this.id2 = i.Price;
+              this.winner2 = i.CompanyName;
+              console.log('Suspensions-winner: ' + i.CompanyName + ' with ' + i.Price + '$/Lot!');
+            }
+          }
+          // tslint:disable-next-line:triple-equals
+          if (i.OccID == 3){
+            if (i.Price <= 30) {
+              this.turbocharges = true;
+              console.log('Turbocharges-boolean: ' + this.turbocharges);
+              this.id3 = i.Price;
+              this.winner3 = i.CompanyName;
+              console.log('Turbocharges-winner: ' + i.CompanyName + ' with ' + i.Price + '$/Lot!');
+            }
+          }
+          // tslint:disable-next-line:triple-equals
+          if (i.OccID == 4){
+            if (i.Price <= 30) {
+              this.exhaustPipe = true;
+              console.log('ExhaustPipe-boolean: ' + this.exhaustPipe);
+              this.id4 = i.Price;
+              this.winner4 = i.CompanyName;
+              console.log('ExhaustPipe-winner: ' + i.CompanyName + ' with ' + i.Price + '$/Lot!');
+            }
+          }
+          // tslint:disable-next-line:triple-equals
+          if (i.OccID == 5){
+            if (i.Price <= 30) {
+              this.brakePads = true;
+              console.log('BrakePads-boolean: ' + this.brakePads);
+              this.id5 = i.Price;
+              this.winner5 = i.CompanyName;
+              console.log('BrakePads-winner: ' + i.CompanyName + ' with ' + i.Price + '$/Lot!');
+            }
+          }
+          // tslint:disable-next-line:triple-equals
+          if (i.OccID == 6){
+            if (i.Price <= 30) {
+              this.rearviewMirrors = true;
+              console.log('RearviewMirrors-boolean: ' + this.rearviewMirrors);
+              this.id6 = i.Price;
+              this.winner6 = i.CompanyName;
+              console.log('RearviewMirrors-winner: ' + i.CompanyName + ' with ' + i.Price + '$/Lot!');
+            }
+          }
+          // tslint:disable-next-line:triple-equals
+          if (i.OccID == 7){
+            if (i.Price <= 30) {
+              this.windshield = true;
+              console.log('Windshield-boolean: ' + this.windshield);
+              this.id7 = i.Price;
+              this.winner7 = i.CompanyName;
+              console.log('Windshield-winner: ' + i.CompanyName + ' with ' + i.Price + '$/Lot!');
+            }
+          }
+        }
       }
     );
+    this.setMin1();
   }
 
   deleteBidder(ID: any): void{
